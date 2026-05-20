4167e306f61cad07aff1c92c457613aded22ea1e
Otto de Maré <otto.demare@alpcot.se>
2026-05-20 09:46:26 +0200

    adding resource group and vnet

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
 terraform {
     required_providers {
-      azurerm = {
-        source = "hashicord/azurerm"
+      azurerm   = {
+        source  = "hashicord/azurerm"
         version = "=4.1.0"
       }
     }
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "Ottos_RG"
+  location = "North Europe"
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "Ottos_VNet"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
+  address_space       = ["10.0.0.0/16"]
 }
