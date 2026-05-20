6c8fa720d0eb7e5f1f64539ff254453b860aaf83
Otto de Maré <otto.demare@alpcot.se>
2026-05-20 10:00:23 +0200

    adding storage account and fixed a typo

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 terraform {
     required_providers {
       azurerm   = {
-        source  = "hashicord/azurerm"
+        source  = "hashicorp/azurerm"
         version = "=4.1.0"
       }
     }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "6a5c7c80-b3f3-4b2f-aacb-8836a0f0aefd"
 }
 
 resource "azurerm_resource_group" "resource_group" {
@@ -17,4 +22,13 @@ resource "azurerm_virtual_network" "virtual_network" {
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
   address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "ottosstorageacc"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_replication_type = "LRS"
+  account_kind             = "BlobStorage"
+  account_tier             = "Standard"
 }
